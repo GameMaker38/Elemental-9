cf06dae6ac3469eee0683c36370d77446d9d938a
GameMaker <76964252+GameMaker38@users.noreply.github.com>
2022-04-21 15:55:27 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let newElement = test;
 let elements = [Air, Earth, Fire, Water];
 let length = elements.length;
-document.getElementById(elements).innerHTML = elements;
-document.getElementById(youMade).innerHTML = "You made " + newElement + "!";
-document.getElementById(youHave).innerHTML = "You have " + elements.length + " elements.";
+document.getElementById("elements").innerHTML = elements;
+document.getElementById("youMade").innerHTML = "You made " + newElement + "!";
+document.getElementById("youHave").innerHTML = "You have " + elements.length + " elements.";
