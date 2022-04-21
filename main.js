e99c7f3f85e0e4461218c3d33fe2348b39fe854f
GameMaker <76964252+GameMaker38@users.noreply.github.com>
2022-04-21 17:33:56 -0600

    Update main.js
    Thanks to charPointer for fixing my code!

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-let newElement = test;
-let elements = [Air, Earth, Fire, Water];
+let newElement = 'test';
+let elements = ['Air', 'Earth', 'Fire', 'Water'];
 let length = elements.length;
-document.getElementById("elements").innerHTML = elements;
-document.getElementById("youMade").innerHTML = "You made " + newElement + "!";
-document.getElementById("youHave").innerHTML = "You have " + elements.length + " elements.";
+document.getElementById("elements").textContent = elements.join(','); 
+document.getElementById("youMade").textContent = `You made ${newElement}!`
+document.getElementById("youHave").textContent = `You have ${length} elements.`;
