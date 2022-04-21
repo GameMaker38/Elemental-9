a1517f8548dc74849b5eaeef1fcf36e5a6c28a5b
GameMaker <76964252+GameMaker38@users.noreply.github.com>
2022-04-21 17:37:31 -0600

    Update main.js

--- main.js
+++ main.js
@@ -4,3 +4,11 @@ let length = elements.length;
 document.getElementById("elements").textContent = elements.join(','); 
 document.getElementById("youMade").textContent = `You made ${newElement}!`
 document.getElementById("youHave").textContent = `You have ${length} elements.`;
+function myFunction() {
+  var x = document.getElementById("settingsDiv");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
