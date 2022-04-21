f261bdb15b11d52a43ffbe1cdc10c336ce4249fc
GameMaker <76964252+GameMaker38@users.noreply.github.com>
2022-04-20 19:39:48 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 let newElement = test
 let elements = [Air, Earth, Fire, Water]
+let length = elements.length;
 document.getElementById("elements").innerHTML = elements
 document.getElementById("youMade").innerHTML = "You made " + newElement + "!";
