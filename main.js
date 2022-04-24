4191f212f27b2c98a675d9cb346e9511ed028331
GameMaker <76964252+GameMaker38@users.noreply.github.com>
2022-04-23 18:27:32 -0600

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,9 @@ let newElement = 'test';
 let elements = ['Air', 'Earth', 'Fire', 'Water'];
 let length = elements.length;
 document.getElementById("elements").textContent = elements.join(','); 
-document.getElementById("youMade").textContent = `You made ${newElement}!`
+function makeNewElement() {
+  document.getElementById("youMade").textContent = `You made ${newElement}!`
+}
 document.getElementById("youHave").textContent = `You have ${length} elements.`;
 function divToggle() {
   var x = document.getElementById("settingsDiv");
