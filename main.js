bb691cf5be54aaf8d92f908da373022f57ff3963
GameMaker <76964252+GameMaker38@users.noreply.github.com>
2022-06-17 15:10:00 -0600

    Hopefully fixed element counter and added notes

--- main.js
+++ main.js
@@ -1,32 +1,33 @@
 let newUsername = '1'
 let color = 'black';
 let newElement = 'test';
-let elements = ['Air', 'Earth', 'Fire', 'Water'];
+// list of your elements
+let yourElements = ['Air', 'Earth', 'Fire', 'Water'];
+// list of all the elements
 let allElements = ['Air', 'Earth', 'Fire', 'Water'];
-let length = elements.length;
+// makes the list of elements appear on the screen
 document.getElementById("elements").textContent = elements.join(','); 
+// gives you an element when you make an existing combo
 function makeNewElement() {
-  document.getElementById("youMade").textContent = `You made ${newElement}!`
+  document.getElementById("youMade").textContent = `You made ${newElement}!`;
+  yourElements.push(var newElement);
 }
-document.getElementById("youHave").textContent = `You have ${length} elements.`;
-function divToggle() {
-  var x = document.getElementById("settingsDiv");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
+document.getElementById("youHave").textContent = `You have ${yourElements.length} out of ${allElements.length} elements.`;
+// looks if a combo exists
+function lookIfComboExists(newElement) {
+  // searches the array for a certain element
+  allElements.find(newElement);
 }
-function lookIfComboExists(combo) {
-  elements.find();
-}
+// adds an element to the game
 function elementAdd() {
-  elements.push(var newElement);
+  // adds the new element to your inventory
+  yourElements.push(var newElement);
+  // adds the new element to the database
+  allElements.push(var newElement);
 }
 function signUp() {
   var newUsername = document.getElementById("signUpUsername");
   accountUsernames.push(var newUsername);
 }
 function logIn() {
-
 }
